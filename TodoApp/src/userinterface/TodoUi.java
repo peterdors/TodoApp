d84f3f2c998a17ce352c11e989b64a9c816b0261
Peter Dorsaneo <peterdorsaneo@gmail.com>
2019-01-06 22:59:01 -0500

    Udpated project.

--- TodoApp/src/userinterface/TodoUi.java
+++ TodoApp/src/userinterface/TodoUi.java
@@ -1,31 +1,29 @@
 package userinterface;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.util.ArrayList;
+
+
 
 import todolist.TodoList;
 
 public class TodoUi 
 {
+	ArrayList<JPanel> panelList;
 	private TodoList todoList;
 	JTextField textField;
-	JButton submitButton;
-	JPanel todoPanel, listPanel;
+	JButton addButton, remButton;
+	JPanel mainPanel, todoPanel, listPanel;
 	JFrame frame;
 	
 	public TodoUi(TodoList todoList)
 	{
 		this.todoList = todoList;
+		panelList = new ArrayList<>();
 		
 		initComponents();
 		
@@ -42,54 +40,110 @@ public class TodoUi
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(700, 700);
-		frame.setVisible(true);
 		
 		initPanelComponents(frame);
+		
+		frame.setLayout(new FlowLayout());
+		
+		frame.setVisible(true);
 		
 	}
 	
 	public void initPanelComponents(JFrame frame)
-	{
+	{	
+		// Initialize the JPanel's.
+		mainPanel = new JPanel();
 		todoPanel = new JPanel();
 		listPanel = new JPanel();
-		
+		// mainPanel settings.
+		mainPanel.setBorder(BorderFactory.createTitledBorder("Title of Main Panel"));
+		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+		// todoPanel settings.
+		todoPanel.setLayout(new GridBagLayout());
+		todoPanel.setPreferredSize(new Dimension(500, 100));
 		todoPanel.setBorder(BorderFactory.createTitledBorder("TODO"));
+		// listPanel settings.
+		listPanel.setLayout(new GridLayout(0, 1));
+		listPanel.setPreferredSize(new Dimension(500, 400));
 		listPanel.setBorder(BorderFactory.createTitledBorder("List of TODO Items"));
-		
-		listPanel.setLayout(new BoxLayout(listPanel, BoxLayout.PAGE_AXIS));
-		listPanel.add(Box.createRigidArea(new Dimension(0,5)));
-		
-		initButtonComponents(frame, todoPanel);
+
 		initTextBoxComponents(todoPanel);
+		initButtonComponents(todoPanel);
 		
-		frame.add(todoPanel, BorderLayout.NORTH);
-		frame.add(listPanel);
+		mainPanel.add(todoPanel);
+		mainPanel.add(listPanel);
 		
+		mainPanel.revalidate();
+		
+		frame.add(mainPanel);
 	}
 	
-	public void initButtonComponents(JFrame frame, JPanel todoPanel)
+	public void initButtonComponents(JPanel todoPanel)
 	{
-		submitButton = new JButton("Submit");
-
-		submitButton.addActionListener(new SubmitButtonListener());
+		addButton = new JButton("Add");
+		remButton = new JButton("Remove");
 		
-		todoPanel.add(submitButton);
+		addButton.setPreferredSize(new Dimension(70, 20));
+		remButton.setPreferredSize(new Dimension(100, 20));
+		
+		addButton.addActionListener(new AddActionListener());
+		remButton.addActionListener(new RemActionListener());
+
+		todoPanel.add(addButton);
+		todoPanel.add(remButton);
+		
+		todoPanel.revalidate();
 	}
 
 	private void initTextBoxComponents(JPanel todoPanel)
 	{
 		textField = new JTextField();
-		todoPanel.add(textField, JPanel.LEFT_ALIGNMENT);
+		textField.setPreferredSize(new Dimension(70, 20));
+		
+		todoPanel.add(textField);
 	}
 	
-	private class SubmitButtonListener implements ActionListener
+	private class AddActionListener implements ActionListener
 	{
 
 		@Override
 		public void actionPerformed(ActionEvent ae) 
 		{
-			// changes
+			String getText = textField.getText();
+			JPanel panel = new JPanel();
+			JLabel label = new JLabel(getText);
+			
+			panel.add(label);
+			
+			panelList.add(panel);
+			
+			listPanel.add(panel);
+			listPanel.validate();
+			listPanel.repaint();
+		}
+		
+	}	
 	
+	private class RemActionListener implements ActionListener
+	{
+
+		@Override
+		public void actionPerformed(ActionEvent ae) 
+		{
+			int index = (panelList.size() > 0) ? (panelList.size() - 1) : 0; 
+			try 
+			{
+				panelList.get(index).removeAll();
+				
+				panelList.subList(index, panelList.size()).clear();
+			} 
+			catch (Exception e)
+			{
+				System.out.println("Empty list");
+			}
+			
+			listPanel.validate();
+			listPanel.repaint();
 		}
 		
 	}
